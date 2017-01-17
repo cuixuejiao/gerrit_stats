157a1afefabecb1eb0f89806260d51f68b03d60d
test <394601129@qq.com>
2017-01-17 16:18:54 +0800

    [BugFix]remove parameters import

--- account.py
+++ account.py
@@ -1,40 +1,39 @@
 # -*- coding: utf-8 -*-
 import pymysql
 
-import params
-
 class Account():
     """ Insert new account into database if account doesn't exist,
-        update account email if it has been changed. """
-    def __init__(self, conn):
+        #update account email if it has been changed. """
+    def __init__(self, conn, name, email):
         self.conn = conn
+        self.name = name
+        self.email = email
+        self.id = 0
     
-    def search_user(self, name):
-        """ Search user in MySQL by name (which should be user id).
-            Return: account_id. """
+    def search_user(self):
+        """ Search user in MySQL by name (which should be user id). """
         with self.conn.cursor() as cursor:
             sql = "SELECT * FROM accounts WHERE full_name=%s"
-            cursor.execute(sql, (name))
+            cursor.execute(sql, (self.name))
             results = cursor.fetchall()
             # The results is a tuple object - ((name, email, account_id),)
             if len(results):
-                self.email = results[0][1]
-                return results[0][2]
-            else:
-                return 0
+                #self.email = results[0][1]
+                self.id = results[0][2]
         
-    def create_user(self, name, email):
+    def create_user(self):
         """ Create new user if he or she doesn't exit. """
         try:
             with self.conn.cursor() as cursor:
                 sql = "INSERT INTO `accounts` (`full_name`, `preferred_email`)\
                        VALUES (%s, %s)"
-                cursor.execute(sql, (name, email))
+                cursor.execute(sql, (self.name, self.email))
             self.conn.commit()
         except:
             self.conn.rollback()
-    
-    def check_email(self, name, email):
+        else:
+            self.search_user()
+    def check_email(self, email):
         """ Update email if it has changed. """
         if self.email != email:
             try:
@@ -46,37 +45,12 @@ class Account():
             except:
                 self.conn.rollback()
     
-    def get_id(self, name, email):
-        """ Get account id. """
-        account_id = self.search_user(name)
-        if account_id:
-            self.check_email(name, email)
-        else:
-            self.create_user(name, email)
-            account_id = self.search_user(name)
-        return account_id
-    
-    def check_user(self):
-        """ Check change owner, patchset uploader and approvals info. """
-        # Get account id.
-        owner_id = self.get_id(params.OWNER_NAME, params.OWNER_EMAIL)
-        uploader_id = self.get_id(params.UPLOADER_NAME, params.UPLOADER_EMAIL)
-        by_0_id = self.get_id(params.BY_0, params.BY_EMAIL_0)
-        by_1_id = self.get_id(params.BY_1, params.BY_EMAIL_1)
-        by_2_id = self.get_id(params.BY_2, params.BY_EMAIL_2)
-        ids = {
-            'OWNER_ACCOUNT_ID': str(owner_id),
-            'UPLOADER_ACCOUNT_ID': str(uploader_id),
-            'BY_0_ID': str(by_0_id),
-            'BY_1_ID': str(by_1_id),
-            'BY_2_ID': str(by_2_id),
-            }
+    def get_account_id(self):
+        """ Return account id. """
+        self.search_user()
+        if self.id == 0:
+            self.create_user()
+        #else:
+        #    self.check_email(email)
         
-        # Write ids into params_file.
-        params_file = 'params.py'
-        try:
-            with open(params_file, 'a') as f_obj:
-                for key, value in sorted(ids.items()):
-                    f_obj.write(key + " = " + value + "\n")
-        except FileNotFuondError:
-            print("Sorry, the file " + params_file + " does not exist.")
+        return self.id
--- change.py
+++ change.py
@@ -1,109 +1,139 @@
 # -*- coding: utf-8 -*-
 import pymysql
+import time
 
-import params
+from account import Account
 
 class Change():
-    """ Write change dict into database. """
-    def __init__(self, conn):
+    """ Insert new change and new accounts into database,
+        update account email if it has been changed. """
+    def __init__(self, conn, dict):
         self.conn = conn
+        # Change info
         self.change_id = 0
+        self.change_key = dict['id']
+        self.project = dict['project']
+        self.branch = dict['branch']
+        # Struct time format (default is timestamp)
+        self.created_on = time.strftime("%Y-%m-%d %H:%M:%S",
+                               time.localtime(dict['createdOn']))
+        # Patchset info
+        self.revision = dict['currentPatchSet']['revision']
+        self.size_insertions = dict['currentPatchSet']['sizeInsertions']
+        self.size_deletions = dict['currentPatchSet']['sizeDeletions']
+        self.approvals = dict['currentPatchSet']['approvals']
+        self.patchset_id = dict['currentPatchSet']['number']
+        # Accounts
+        self.owner_id = 0
+        self.owner_name = dict['owner']['name']
+        self.owner_email = dict['owner']['email']
+        self.uploader_id = 0
+        self.uploader_name = dict['currentPatchSet']['uploader']['name']
+        self.uploader_email = dict['currentPatchSet']['uploader']['email']
     
-    def get_change_id(self, change_key):
-        """ Get change id in MySQL by change_key. """
+    def search_change(self):
+        """ Get change id in database. """
         with self.conn.cursor() as cursor:
             sql = "SELECT `change_id` FROM `changes` WHERE change_key=%s"
-            cursor.execute(sql, (change_key))
+            cursor.execute(sql, (self.change_key))
             results = cursor.fetchall()
             # The results is a tuple object - ((change_id),)
             if len(results):
                 self.change_id = results[0][0]
     
+        
     def create_change(self):
         """ Insert new gerrit change into database. """
         try:
             with self.conn.cursor() as cursor:
                 sql = "INSERT INTO `changes` (`change_key`, \
                                               `created_on`, \
-                                              `last_updated_on`, \
                                               `owner_account_id`, \
                                               `dest_project_name`, \
                                               `dest_branch_name`, \
                                               `status`, \
                                               `current_patch_set_id` ) \
-                       VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-                cursor.execute(sql, (params.CHANGE_KEY,
-                                     params.CREATED_ON,
-                                     params.LAST_UPDATED_ON,
-                                     params.OWNER_ACCOUNT_ID,
-                                     params.DEST_PROJECT_NAME,
-                                     params.DEST_BRANCH_NAME,
-                                     'M',
-                                     params.CURRENT_PATCH_SET_ID))
+                       VALUES (%s, %s, %s, %s, %s, 'M', %s)"
+                cursor.execute(sql, (self.change_key,
+                                     self.created_on,
+                                     self.owner_id,
+                                     self.project,
+                                     self.branch,
+                                     self.patchset_id))
             self.conn.commit()
         except:
             self.conn.rollback()
-        # Write change_id into params_file.
-        self.get_change_id(params.CHANGE_KEY)
+        else:
+            # Set change id.
+            self.search_change()
     
     def create_patch_set(self):
         """ Insert new gerrit change patchset into database. """
         try:
             with self.conn.cursor() as cursor:
-                sql = "INSERT INTO `patch_sets` (`revision`, \
-                                                 `uploader_account_id`, \
-                                                 `size_insertions`, \
-                                                 `size_deletions`, \
-                                                 `change_id`, \
-                                                 `patch_set_id` ) \
+                sql = "INSERT INTO `patch_sets` (\
+                                    `revision`,\
+                                    `uploader_account_id`,\
+                                    `size_insertions`,\
+                                    `size_deletions`,\
+                                    `change_id`,\
+                                    `patch_set_id` )\
                        VALUES ( %s, %s, %s, %s, %s, %s)"
-                cursor.execute(sql, (params.REVISION,
-                                     params.UPLOADER_ACCOUNT_ID,
-                                     params.SIZE_INSERTIONS,
-                                     params.SIZE_DELETIONS,
+                cursor.execute(sql, (self.revision,
+                                     self.uploader_id,
+                                     self.size_insertions,
+                                     self.size_deletions,
                                      self.change_id,
-                                     params.CURRENT_PATCH_SET_ID))
+                                     self.patchset_id))
             self.conn.commit()
         except:
             self.conn.rollback()
     
     def create_patch_set_approvals(self):
-        """ Insert new gerrit change patchset approvals into database. """
-        try:
-            with self.conn.cursor() as cursor:
-                sql = "INSERT INTO `patch_set_approvals` (`value`, \
-                                                          `granted`, \
-                                                          `change_id`, \
-                                                          `patch_set_id`, \
-                                                          `account_id`, \
-                                                          `category_id`) \
-                       VALUES (%s, %s, %s, %s, %s, %s)"
-                cursor.execute(sql, (params.VALUE_0,
-                                     params.GRANTED_0,
-                                     self.change_id,
-                                     params.CURRENT_PATCH_SET_ID,
-                                     params.BY_0_ID,
-                                     params.CATEGORY_ID_0))
-                cursor.execute(sql, (params.VALUE_1,
-                                     params.GRANTED_1,
-                                     self.change_id,
-                                     params.CURRENT_PATCH_SET_ID,
-                                     params.BY_1_ID,
-                                     params.CATEGORY_ID_1))
-                cursor.execute(sql, (params.VALUE_2,
-                                     params.GRANTED_2,
-                                     self.change_id,
-                                     params.CURRENT_PATCH_SET_ID,
-                                     params.BY_2_ID,
-                                     params.CATEGORY_ID_2))
-            self.conn.commit()
-        except:
-            self.conn.rollback()
+        """ Insert new gerrit change patchset approvals into database. """                
+        for approval in self.approvals:
+            # There could be multi-reivewers.
+            if approval['type'] == 'Code-Review':
+                r_value = approval['value']
+                r_granted = time.strftime("%Y-%m-%d %H:%M:%S",
+                                 time.localtime(approval['grantedOn']))
+                r_name = approval['by']['name']
+                r_email = approval['by']['email']
+                r = Account(self.conn, r_name, r_email)
+                r_id = r.get_account_id()
+                try:
+                    with self.conn.cursor() as cursor:
+                        sql = "INSERT INTO `patch_set_approvals` (\
+                                            `value`,\
+                                            `granted`,\
+                                            `change_id`,\
+                                            `patch_set_id`,\
+                                            `account_id`,\
+                                            `category_id`)\
+                               VALUES (%s, %s, %s, %s, %s, 'Code-Review')"
+                        cursor.execute(sql, (r_value,
+                                             r_granted,
+                                             self.change_id,
+                                             self.patchset_id,
+                                             r_id))
+                    self.conn.commit()
+                except:
+                    self.conn.rollback()
     
     def check_change(self):
-        """ Create new change into database if it doesn't exist. """
-        self.get_change_id(params.CHANGE_KEY)
+        """ Create new change into database. """
+        # Get change owner, patchset uploader and approvals account id.
+        owner = Account(self.conn, self.owner_name, self.owner_email)
+        self.owner_id = owner.get_account_id()
+        uploader = Account(self.conn, self.uploader_name, self.uploader_email)
+        self.uploader_id = uploader.get_account_id()
+        
+        # Set change info
+        self.search_change()
         if self.change_id == 0:
             self.create_change()
             self.create_patch_set()
             self.create_patch_set_approvals()
+            print("[Info] insert new change into statisdb: %s" % self.change_key)
+        else:
+            print("The change: %s has already exist." % self.change_key)
